4bb4780924ed1e10f1ae02079f22904c8a6107f7
Farrokh <farrokhandarz84@gmail.com>
2026-07-06 03:10:38 +0330

    add a new fetch for get artis albums

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 async function getArtistAlbums() {
-  const artistName = "joji";
+  const artistName = "Joji";
   const searchUrl = `https://itunes.apple.com/search?term=${artistName}&entity=musicArtist`;
   const response = await fetch(searchUrl);
   const data = await response.json();
+  const artistId = data.results[0].artistId;
+  const lookupUrl = `https://itunes.apple.com/lookup?id=${artistId}&entity=album`;
+  const responseartistId = await fetch(lookupUrl);
+  const dataArtistid = await responseartistId.json();
   console.log(data);
+  console.log(dataArtistid);
 }
 
 getArtistAlbums();
