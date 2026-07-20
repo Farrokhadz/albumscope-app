7ee7d707bdad6666d8fd4c39a69885e79f8af148
Farrokh <farrokhandarz84@gmail.com>
2026-07-21 02:51:02 +0330

    add a new table for db

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,10 @@
 const Database = require("better-sqlite3");
 const db = new Database("playhead.db");
 db.exec(`
- CREATE TABLE IF NOT EXISTS users ( id INTEGER  PRIMARY KEY AUTOINCREMENT , email TEXT, password TEXT)
+ CREATE TABLE IF NOT EXISTS users ( id INTEGER  PRIMARY KEY AUTOINCREMENT , email TEXT UNIQUE, password TEXT)
 `);
 const stmt = db.prepare("INSERT INTO users (email,password) VALUES (?, ?)");
 stmt.run("Farrokhandarz84@gmail.com", "123456789");
+
+const rows = db.prepare("SELECT * FROM users").all();
+console.log(rows);
