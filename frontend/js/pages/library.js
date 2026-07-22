20424199bfdadeda85f2f4abbb554f3383d88c90
Farrokh <farrokhandarz84@gmail.com>
2026-07-22 22:20:54 +0330

    add search.js , library.js , profile.js for spa in project

---
+++ frontend/js/pages/library.js
@@ -0,0 +1,6 @@
+function renderLibraryPage() {
+  return `
+    <h2 class="section-title">Your Library</h2>
+    <p>هنوز آلبومی ذخیره نکردی.</p>
+  `;
+}
---
+++ frontend/js/pages/profile.js
@@ -0,0 +1,6 @@
+function renderProfilePage() {
+  return `
+    <h2 class="section-title">Profile</h2>
+    <p>اینجا اطلاعات کاربر نشون داده می‌شه.</p>
+  `;
+}
